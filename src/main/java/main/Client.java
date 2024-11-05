4f32aec3ab357e0f9a9e03f1d622b66bdb5ee96d
Sergey <52660922+Angel-Windows@users.noreply.github.com>
2024-11-05 21:22:02 +0200

    По лабе

--- src/main/java/main/Client.java
+++ src/main/java/main/Client.java
@@ -1,34 +1,40 @@
 package main;
 import eduni.simjava.*;
+
 public class Client extends Sim_entity {
     private Sim_port in, out;
     private double delay;
-    public static int jobs_count=3;
+    public static int jobs_count = 2; // Установлено количество задач
+
     Client(String name, double delay) {
         super(name);
-        this.delay=delay;
+        this.delay = delay;
         in = new Sim_port("In");
         out = new Sim_port("Out");
         add_port(in);
         add_port(out);
     }
+
     @Override
     public void body() {
-        for(int i=0; i<jobs_count; i++) {
-            sim_schedule(out, 3.0, i);
-            sim_trace(Sim_system.get_trc_level(),
-                    "Job "+i+" from client has been sent...");
-            sim_pause(delay);
+        // Отправляем первую задачу
+        sim_schedule(out, 0.0, 0);
+        sim_trace(Sim_system.get_trc_level(), "jdl-file has been sent...");
+        sim_pause(10.0); // Пауза для согласования времени отправки
+
+        for (int i = 1; i < jobs_count; i++) {
+            sim_schedule(out, 3.0 + (i * 3.0), i); // Каждая следующая задача
+            sim_trace(Sim_system.get_trc_level(), "jdl-file has been sent...");
         }
+
+        // Ожидание результата
         Sim_event event = new Sim_event();
         sim_wait(event);
-        if(event.from_port(in) && CE.link) {
-            CE.link=false;
-            sim_trace(Sim_system.get_trc_level(),
-                    ">> Result link received!");
+        if (event.from_port(in) && CE.link) {
+            CE.link = false;
+            sim_trace(Sim_system.get_trc_level(), ">> Result link received!");
             System.out.println("Done!");
             sim_completed(event);
-
         }
     }
 }
--- src/main/java/main/Grid.java
+++ src/main/java/main/Grid.java
@@ -1,15 +1,16 @@
 package main;
 import eduni.simjava.*;
+
 public class Grid {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Sim_system.initialise();
-        Client client = new Client("Client", 10.0);
+        Client client = new Client("Client", 2.0);
         CE ce = new CE("CE", 2.0);
         WN wn = new WN("WN", 50.0);
-        Sim_system.link_ports("Client","Out","CE","In1");
-        Sim_system.link_ports("CE","Out1","WN","In");
-        Sim_system.link_ports("WN","Out","CE","In2");
-        Sim_system.link_ports("CE","Out2","Client","In");
+        Sim_system.link_ports("Client", "Out", "CE", "In1");
+        Sim_system.link_ports("CE", "Out1", "WN", "In");
+        Sim_system.link_ports("WN", "Out", "CE", "In2");
+        Sim_system.link_ports("CE", "Out2", "Client", "In");
         Sim_system.set_trace_detail(false, true, false);
         Sim_system.run();
     }
